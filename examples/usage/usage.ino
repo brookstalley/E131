6b04f8c66707c7e1bfb46d0ed54b8b19f57bdda2
AdmiralTriggerHappy <andrew@asw.id.au>
2017-07-10 14:57:08 +1000

    Update usage.ino

--- examples/usage/usage.ino
+++ examples/usage/usage.ino
@@ -6,7 +6,7 @@ SYSTEM_THREAD(ENABLED);
 // Initialize objects from the lib
 E131 e131;
 
- //Test Code
+	//Setup LED control pins
     int blueLED = D0;
     int greenLED = D1;
     int redLED = D2;
@@ -14,13 +14,13 @@ E131 e131;
 void setup() {
     // Call functions on initialized library objects that require hardware
     
-	 //Test Code
-    // Here's the pin configuration, same as last time
+    // Set the LED control pin to output
    pinMode(blueLED, OUTPUT);
    pinMode(redLED, OUTPUT);
    pinMode(greenLED, OUTPUT);
 
-   // For good measure, let's also make sure both LEDs are off when we start:
+   // For good measure, let's also make sure all LEDs are off when we start:
+   //In this example Common Cathode RGB LED is used so High is off.
    digitalWrite(blueLED, HIGH);
    digitalWrite(redLED, HIGH);
    digitalWrite(greenLED, HIGH);
@@ -33,12 +33,16 @@ void setup() {
 void loop() {
 
 	e131.parsePacket();
-	//Serial.printlnf("## data= %d , %d, %d", e131.data[0],e131.data[1],e131.data[2]);
-	redBright(e131.data[0]);
-       greenBright(e131.data[1]);
-       blueBright(e131.data[2]);
+	
+		//Use the first 3 channels to control the Red, Green and Blue LED respectively
+	   redBright(e131.data[1]);
+       greenBright(e131.data[2]);
+       blueBright(e131.data[3]);
 
 }
+
+//Some functions to set the brightness for each of the LEDs, as LEDs are common Cathode brightness value is inverted.
+
  int redBright(int brightness) {
         brightness = 255 - brightness;
         analogWrite(redLED,brightness);
